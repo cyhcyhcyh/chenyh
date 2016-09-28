2c6ade56a4df0e87e6f2c434352a227e9791baab
ios18 <ios18@localhost>
2016-09-28 12:04:31 +0800

    实体类封装上传

--- src/com/chenyh/accountms/model/tb_IncomeInfo.java
+++ src/com/chenyh/accountms/model/tb_IncomeInfo.java
@@ -1,4 +1,6 @@
 package com.chenyh.accountms.model;
+
+import android.R.interpolator;
 //输入信息实体类
 public class tb_IncomeInfo {
 	//默认的构造函数
@@ -6,6 +8,75 @@ public class tb_IncomeInfo {
 		
 	}
 	// 定义有参构造函数，用来初始化用户实体类中的各个字段
-	public tb_IncomeInfo(int IncomeID,int ID,int TypeID,Double Money,String Time  ){
+	public tb_IncomeInfo(int IncomeID,int ID,int TypeID,Double Money,String Time,String payment,String Address,String Depict){
+		this.IncomeID=IncomeID;
+		this.ID=ID;
+		this.TypeID=TypeID;
+		this.Money=Money;
+		this.Time=Time;
+		this.payment=payment;
+		this.Address=Address;
+		this.Depict=Depict;
+		
 	}
+	private int IncomeID;//收入编号
+	private int ID;//成员编号
+	private int TypeID;//收入类别
+	private double Money;//输入金额
+	private String Time;//输入时间
+	private String payment;//收款方式
+	private String Address;//输入地点
+	private String Depict;//输入备注
+	
+	//设置输入编号的可读属性
+	public int getIncomeID() {
+		return IncomeID;
+	}
+	
+	public void setIncomeID(int incomeID) {
+		IncomeID = incomeID;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getTypeID() {
+		return TypeID;
+	}
+	public void setTypeID(int typeID) {
+		TypeID = typeID;
+	}
+	public double getMoney() {
+		return Money;
+	}
+	public void setMoney(double money) {
+		Money = money;
+	}
+	public String getTime() {
+		return Time;
+	}
+	public void setTime(String time) {
+		Time = time;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getDepict() {
+		return Depict;
+	}
+	public void setDepict(String depict) {
+		Depict = depict;
+	}
+
 }
--- src/com/chenyh/accountms/model/tb_PayInfo.java
+++ src/com/chenyh/accountms/model/tb_PayInfo.java
@@ -1,13 +1,111 @@
 package com.chenyh.accountms.model;
+
+import android.R.string;
+
 //支出信息实体类
 public class tb_PayInfo {
 	//默认的构造函数
 	public tb_PayInfo(){
 		
 	}
-	/*//
-	public tb_PayInfo(int PayID,int ID,int TypeID ,double Money,){
+	// 定义有参构造函数，用来初始化支出信息实体类中的各个字段
+	public tb_PayInfo(int PayID,int ID,int TypeID ,double Money,String Time,String Payment,String Address,String Depict){
+		this.PayID=PayID;
+		this.ID=ID;
+		this.TypeID=TypeID;
+		this.Money=Money;
+		this.Time=Time;
+		this.Payment=Payment;
+		this.Address=Address;
+		this.Depict=Depict;
 		
-	}*/
+	}
+	private int PayID;//支出编号
+	private int ID;//成员编号
+	private int TypeID;//支出类别
+	private double Money;//金额
+	private String Time;//时间
+	private String Payment;//付款方式
+	private String Address;//地点
+	private String Depict;//备注
+	
+	//设置支出编号的可读属性
+	public int getPayID() {
+		return PayID;
+	}
+	
+	//设置支出编号的可写属性
+	public void setPayID(int payID) {
+		PayID = payID;
+	}
+	
+	//设置成员编号的可读属性
+	public int getID() {
+		return ID;
+	}
 
+	//设置成员编号的可写属性
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	//设置支出类别的可读属性
+	public int getTypeID() {
+		return TypeID;
+	}
+
+	//设置支出类别的可写属性
+	public void setTypeID(int typeID) {
+		TypeID = typeID;
+	}
+
+	//设置支付金额的可读属性
+	public double getMoney() {
+		return Money;
+	}
+
+	//设置支付金额的可写属性
+	public void setMoney(double money) {
+		Money = money;
+	}
+
+	//设置支付时间的可读属性
+	public String getTime() {
+		return Time;
+	}
+
+	//设置支付时间的可写属性
+	public void setTime(String time) {
+		Time = time;
+	}
+
+	//设置付款方式的可读属性
+	public String getPayment() {
+		return Payment;
+	}
+
+	//设置付款方式的可写属性
+	public void setPayment(String payment) {
+		Payment = payment;
+	}
+
+	//设置支付地点的可读属性
+	public String getAddress() {
+		return Address;
+	}
+
+	//设置支付地点的可写属性
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	//设置支付备注的可读属性
+	public String getDepict() {
+		return Depict;
+	}
+
+	//设置支付备注的可写属性
+	public void setDepict(String depict) {
+		Depict = depict;
+	}
 }
