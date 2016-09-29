28a71a27afefa083584044ead8fbb075686a5ade
ios18 <ios18@localhost>
2016-09-29 10:35:19 +0800

    表上传

--- src/com/chenyh/accountms/db/DBOpenHelper.java
+++ src/com/chenyh/accountms/db/DBOpenHelper.java
@@ -19,11 +19,26 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 	}
 	
 	/**
-     * 初次创建数据库时才会被调用
+     * 初次创建数据库时才会被调用(创建用户表,家庭成员表,便签表,输入信息表,输入类别表,支出信息表,支出类别表)
      * @param db
      */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
+		 String sql = "create table if not exists tb_User "  
+	                + "(UserID integer primary key autoincrement,UserName varchar(32),Password varchar(100),Depict varchar(200)) go" +
+	                "create table if not exists tb_Family" 
+	                + "(ID integer primary key autoincrement,Name varchar(32),Depict varchar(200)) go"+ 
+	                "create table if not exists tb_Flag"
+	                + "(FlagID integer primary key autoincrement,Title varchar(50),Content varchar(200),Time varchar(50)) go"+
+	                "create table if not exists tb_IncomeInfo"
+	                + "(IncomeID integer primary key autoincrement,ID,TypeID,Money decimal,Time varchar(50),payment varchar (50),Depict varchar(200)) go"+
+	                "create table if not exists tb_IncomeType"
+	                +"(TypeID integer primary key autoincrement,TypeName varchar(30),Depict varchar(200)) go"+
+	                "create table if not exists tb_PayInfo"
+	                +"(PayID integer primary key autoincrement,ID,TypeID,Money decimal,Time,varchar(50),Payment,varchar (50),Address varchar(50),Depict varchar(200)) go"+
+	                "create table if not exists tb_PayType"
+	                +"(TypeID integer primary key autoincrement,TypeName varchar(30),Depict varchar(200))";  
+	        db.execSQL(sql);
 	}
 	 /**
      * 数据库的版本发生变化时会被调用
--- src/com/chenyh/accountms/model/tb_PayInfo.java
+++ src/com/chenyh/accountms/model/tb_PayInfo.java
@@ -1,7 +1,4 @@
 package com.chenyh.accountms.model;
-
-import android.R.string;
-
 //支出信息实体类
 public class tb_PayInfo {
 	//默认的构造函数
