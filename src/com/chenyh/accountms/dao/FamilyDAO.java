467eb78b0dc3cb4c5c3de97b11c07752f6479463
ios17 <ios17@localhost>
2016-09-29 15:25:57 +0800

    家庭成员和收入信息

--- src/com/chenyh/accountms/dao/FamilyDAO.java
+++ src/com/chenyh/accountms/dao/FamilyDAO.java
@@ -1,7 +1,10 @@
 package com.chenyh.accountms.dao;
 
 import com.chenyh.accountms.db.DBOpenHelper;
+import com.chenyh.accountms.model.tb_Family;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class FamilyDAO {
@@ -12,9 +15,70 @@ public class FamilyDAO {
 	{
 		helper = new DBOpenHelper(context);// 初始化DBOpenHelper对象
 	}
-	
-	private void insert() {
-		// TODO Auto-generated method stub
 
+	
+	/***
+	 * 添加家庭成员信息
+	 * @param tb_Family
+	 */
+	public void  insert(tb_Family tb_Family)
+	{
+		db=helper.getWritableDatabase();//初始化SQLiteDatabase
+		String sql="insert into tb_Family(ID,Name,Depict) values(?,?,?)";
+		db.execSQL(sql, new Object[]{
+						tb_Family.getID(),
+						tb_Family.getName(),
+						tb_Family.getDepict()});
+		
 	}
+	
+	/***
+	 * 更新家庭成员信息
+	 * @param tb_Family
+	 */
+	public  void update(tb_Family tb_Family)
+	{
+		db=helper.getWritableDatabase();
+		String sql="update tb_Family set Name=?,Depict=? where ID=?";
+		db.execSQL(sql, new Object[]{
+						tb_Family.getName(),
+						tb_Family.getDepict(),
+						tb_Family.getID()});
+	}
+	
+	
+	/**
+	 * 删除家庭成员信息
+	 * @param tb_Family
+	 */
+	public void delete(tb_Family tb_Family)
+	{
+		db=helper.getWritableDatabase();
+		String sql="delete from tb_Family where ID=?";
+		db.execSQL(sql, new Object[]{
+						tb_Family.getID()});
+		
+	}
+	
+	/**
+	 * 查找家庭成员信息
+	 * @return tb_Family
+	 */
+	 public tb_Family selectAll()
+	 {
+		 db=helper.getWritableDatabase();
+		 String sql=" select * from tb_Family";
+		 
+		 Cursor  cursor=db.rawQuery(sql,null);// 查找所有家庭成员信息，并存储到Cursor类中
+		 if(cursor.moveToNext())//遍历查找到的家庭成员信息
+		 {
+			// 将遍历到的便签信息存储到tb_Family类中
+			 return new tb_Family(cursor.getInt(cursor.getColumnIndex("ID")),
+					 cursor.getString(cursor.getColumnIndex("Name")),
+					 cursor.getString(cursor.getColumnIndex("Depict")));
+		 }
+		 
+		return null;//如果没有信息，则返回null
+	 }
+	
 }
--- src/com/chenyh/accountms/dao/IncomeInfoDAO.java
+++ src/com/chenyh/accountms/dao/IncomeInfoDAO.java
@@ -1,7 +1,12 @@
 package com.chenyh.accountms.dao;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import com.chenyh.accountms.db.DBOpenHelper;
+import com.chenyh.accountms.model.tb_IncomeInfo;
 public class IncomeInfoDAO {
 	private DBOpenHelper helper;// 创建DBOpenHelper对象
 	private SQLiteDatabase db;// 创建SQLiteDatabase对象
@@ -11,4 +16,99 @@ public class IncomeInfoDAO {
 		helper = new DBOpenHelper(context);// 初始化DBOpenHelper对象
 	}
 
+	
+
+	/***
+	 * 新增收入信息
+	 * @param tb_IncomeInfo
+	 */
+	public void insert(tb_IncomeInfo tb_IncomeInfo)
+	{
+		db=helper.getWritableDatabase();
+		String sql="insert into tb_IncomeInfo( IncomeID, ID, TypeID, Money, Time, payment, Depict) values(?,?,?,?,?,?,?)";
+		db.execSQL(sql, new Object[]{
+				tb_IncomeInfo.getIncomeID(),
+				tb_IncomeInfo.getID(),
+				tb_IncomeInfo.getTypeID(),
+				tb_IncomeInfo.getMoney(),
+				tb_IncomeInfo.getTime(),
+				tb_IncomeInfo.getPayment(),
+				tb_IncomeInfo.getDepict()});
+	}
+	
+	/**
+	 * 更新收入信息
+	 * @param tb_IncomeInfo
+	 */
+	public void update(tb_IncomeInfo tb_IncomeInfo)
+	{
+		db=helper.getWritableDatabase();
+		String sql="update tb_IncomeInfo set ID=?," +
+										"TypeID=?," +
+										"Money=?," +
+										"Time=?, " +
+										"payment=?, " +
+										"Depict=?" +
+										"where IncomeID=? ";
+		db.execSQL(sql, new Object[]{
+				tb_IncomeInfo.getID(),
+				tb_IncomeInfo.getTypeID(),
+				tb_IncomeInfo.getMoney(),
+				tb_IncomeInfo.getTime(),
+				tb_IncomeInfo.getPayment(),
+				tb_IncomeInfo.getDepict(),
+				tb_IncomeInfo.getIncomeID()});
+	}
+	
+	/**
+	 * 删除收入信息
+	 * @param tb_IncomeInfo
+	 */
+	public void delete(tb_IncomeInfo tb_IncomeInfo)
+	{
+		db=helper.getWritableDatabase();
+		String sql="delete from tb_IncomeInfo where IncomeID=?";
+		db.execSQL(sql, new Object[]{
+				tb_IncomeInfo.getIncomeID()});
+		
+		
+	}
+	
+	/**
+	 * 获取收入信息
+	 * 
+	 * @param start
+	 *            起始位置
+	 * @param count
+	 *            每页显示数量
+	 * @return
+	 */
+	public List<tb_IncomeInfo> getScrollData(int start, int count) {
+		List<tb_IncomeInfo> lisTb_flags = new ArrayList<tb_IncomeInfo>();// 创建集合对象
+		db = helper.getWritableDatabase();// 初始化SQLiteDatabase对象
+		// 获取所有便签信息
+		Cursor cursor = db.rawQuery("select * from tb_IncomeInfo limit ?,?",
+				new String[] { String.valueOf(start), String.valueOf(count) });
+		while (cursor.moveToNext())// 遍历所有的便签信息
+		{
+			// 将遍历到的便签信息添加到集合中
+			lisTb_flags.add(new tb_IncomeInfo());
+		}
+		return lisTb_flags;// 返回集合
+	}
+
+	/**
+	 * 获取总记录数
+	 * 
+	 * @return
+	 */
+	public long getCount() {
+		db = helper.getWritableDatabase();// 初始化SQLiteDatabase对象
+		Cursor cursor = db.rawQuery("select count(IncomeID) from tb_IncomeInfo", null);// 获取便签信息的记录数
+		if (cursor.moveToNext())// 判断Cursor中是否有数据
+		{
+			return cursor.getLong(0);// 返回总记录数
+		}
+		return 0;// 如果没有数据，则返回0
+	}
 }
