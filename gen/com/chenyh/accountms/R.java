7a0cf5f5d3ad1f7694a73f949a19934244437f86
ios17 <ios17@localhost>
2016-09-30 15:05:29 +0800

    测试注册

--- gen/com/chenyh/accountms/R.java
+++ gen/com/chenyh/accountms/R.java
@@ -23,9 +23,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int login_logo=0x7f020001;
+        public static final int setbar_bg=0x7f020002;
+        public static final int zhuc_f=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int ETpwd=0x7f080003;
+        public static final int ETpwdok=0x7f080004;
+        public static final int ETusername=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int btnregister=0x7f080005;
+        public static final int imglogin_logo=0x7f080001;
+        public static final int zhuc=0x7f080000;
     }
     public static final class layout {
         public static final int activity_family=0x7f030000;
--- src/com/chenyh/accountms/activity/MainActivity.java
+++ src/com/chenyh/accountms/activity/MainActivity.java
@@ -4,35 +4,55 @@ import com.chenyh.accountms.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.chenyh.accountms.dao.UserDAO;
+import com.chenyh.accountms.model.tb_User;
 public class MainActivity extends Activity {
 
+	private EditText UserName;
+	private EditText Password;
+	private EditText PwdOK;
+	private Button  btnregister;
+	
+	
+	UserDAO userDAO=new UserDAO(null);
+    tb_User User=new tb_User();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+        
+       
+        UserName=(EditText) findViewById(R.id.ETusername);
+        Password=(EditText) findViewById(R.id.ETpwd);
+        PwdOK=(EditText) findViewById(R.id.ETpwdok);
+        
+        User.setUserName(UserName.getText().toString());
+        User.setPassword(Password.getText().toString());
+        
+        btnregister=(Button) findViewById(R.id.btnregister);
+        		btnregister.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View arg0) {
+						 if(Password.getText().toString()!=PwdOK.getText().toString())
+					        {
+					        		userDAO.insert(User);
+					        		Toast.makeText(MainActivity.this, "注册成功！", Toast.LENGTH_LONG).show();
+					        }
+					        else {
+								Toast.makeText(MainActivity.this, "密码与确认密码不一致！请重新输入！", Toast.LENGTH_LONG).show();
+								return ;
+							}
+						
+					}
+				});
+       
+        
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
